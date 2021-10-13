213aa1dd16ddd103ce86943eaef92d8a9252f700
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-10-13 18:14:02 +0300

    updated navbar

--- src/navigation.js
+++ src/navigation.js
@@ -3,16 +3,18 @@ const softBlack = 'rgb(15, 15, 15)'
 const solftBlue = 'rgb(35, 170, 222)'
 const nav = document.getElementById('navbar')
 const logo = document.querySelector('.logo-image')
-const navLinksMenu = document.getElementById('#nav-links')
 const navLinks = document.getElementsByClassName('nav-link')
 const navMenuLines = document.getElementsByClassName('line')
 
 window.addEventListener('resize', () => {
+    // navbar is on TOP on a device with MAX width of 770px
     if (window.matchMedia('(max-width: 770px)').matches && window.scrollY === 0) {
         for (let link = 0; link < navLinks.length; link++) {
             navLinks[link].style.transition = 'all 0.5s'
             navLinks[link].style.color = softBlack
         }
+
+    // navbar is on TOP on a device with MIN width of 770px
     } else if (window.matchMedia('(min-width: 770px)').matches && window.scrollY === 0) {
         for (let link = 0; link < navLinks.length; link++) {
             navLinks[link].style.transition = 'all 0.5s'
@@ -22,6 +24,7 @@ window.addEventListener('resize', () => {
 })
 
 window.addEventListener('scroll', () => {
+    // navbar scrolling down
     if (window.scrollY > 0) {
         nav.classList.add('navbar-scrolling')
         logo.style.content = 'url(/images/Logo.png)'
@@ -30,18 +33,35 @@ window.addEventListener('scroll', () => {
             navLinks[link].style.transition = 'all 0.5s'
             navLinks[link].style.color = softBlack
         }
+
         for (let line = 0; line < navMenuLines.length; line++) {
             navMenuLines[line].style.backgroundColor = softBlack
         }
+
+        // navbar scrolling down on a device with MIN width of 770px
+        if (window.matchMedia('(min-width: 770px)').matches) {
+            nav.classList.add('nav-scrolling-resize')
+            logo.style.width = '20%'
+        }
+
+    // navbar is on TOP
     } else if (window.scrollY === 0) {
         nav.classList.remove('navbar-scrolling')
         logo.style.content = 'url(/images/Logo-T.png)'
 
+        if (window.matchMedia('(min-width: 770px)').matches && window.scrollY === 0) {
+            nav.classList.remove('nav-scrolling-resize')
+            logo.style.width = '30%'
+        }
+
+        // navbar is on TOP on a device with MAX width of 770px
         if (window.matchMedia('(max-width: 770px)').matches && window.scrollY === 0) {
             for (let link = 0; link < navLinks.length; link++) {
                 navLinks[link].style.transition = 'all 0.5s'
                 navLinks[link].style.color = softBlack
             }
+
+        // navbar is on top on a device with MIN width of 770px
         } else {
             for (let link = 0; link < navLinks.length; link++) {
                 navLinks[link].style.transition = 'all 0.5s'
