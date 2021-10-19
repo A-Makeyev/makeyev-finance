6ac4e6a710f4fb095c7df75b6800a4f906d9d7b6
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-10-19 13:19:05 +0300

    removed body variable

--- src/navigation.js
+++ src/navigation.js
@@ -1,7 +1,7 @@
 const softWhite = 'aliceblue'
 const softBlack = 'rgb(15, 15, 15)'
 const solftBlue = 'rgb(35, 170, 222)'
-const body = document.querySelector('body')
+
 const menu = document.getElementById('menu')
 const nav = document.getElementById('navbar')
 const logo = document.querySelector('.logo-image')
@@ -9,6 +9,7 @@ const navLinks = document.getElementsByClassName('nav-link')
 const navMenuLines = document.getElementsByClassName('line')
 
 menu.onclick = () => {
+    let body = document.querySelector('body')
     body.classList.contains('stop-scrolling') 
     ? body.classList.remove('stop-scrolling')
     : body.classList.add('stop-scrolling')
