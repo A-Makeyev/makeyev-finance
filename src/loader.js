5fcf679ef25e93171e01149b3f3c46e07d4a136e
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-10-19 13:18:25 +0300

    added loading animation

---
+++ src/loader.js
@@ -0,0 +1,30 @@
+const loader = document.getElementById('loader')
+const loadingScreen = document.getElementById('loading-screen')
+
+const hidden = document.createElement('style')
+hidden.innerHTML = '.hidden { display: none !important; }'
+document.getElementsByTagName('head')[0].appendChild(hidden)
+
+function loadMainPage() {
+    loadingScreen.style.opacity = '0'
+    document.querySelector('body').classList.remove('stop-scrolling')
+
+    setTimeout(() => {
+        loadingScreen.classList.add('hidden')
+    }, 1000)
+}
+
+if (window.attachEvent) {
+    window.attachEvent('onload', loadMainPage)
+} else {
+    if (window.onload) {
+        var curronload = window.onload
+        var newonload = (event) => {
+            curronload(event)
+            loadMainPage()
+        }
+        window.onload = newonload
+    } else {
+        window.onload = loadMainPage
+    }
+}
