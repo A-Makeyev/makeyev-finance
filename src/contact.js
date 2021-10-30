f28cf67f153eb1b4f9d784aaeeb76de31d23f6cb
anatolymakeyev <anatoly.makeyev@cloudbeat.io>
2021-10-30 23:53:34 +0200

    removed useless code

--- src/contact.js
+++ src/contact.js
@@ -1,19 +1,3 @@
 const submit = document.getElementById('submit')
-submit.addEventListener('click', submitForm())
-
-function submitForm(event) {
-    event.preventDefault()
-
-    let name = document.getElementById('name')
-    let email = document.getElementById('email')
-    let phone = document.getElementById('phone')
-    let message = document.getElementById('message')
-    
-    alert(name + ' ' + email + ' ' + phone + ' ' + message)
-
-    document.getElementById('contact-form').reset()
-
-}
-
 
 
