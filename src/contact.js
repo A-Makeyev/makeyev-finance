bc97339040dff857589f36c340ba4ad3c3992f26
anatolymakeyev <anatoly.makeyev@cloudbeat.io>
2021-11-01 20:23:02 +0100

    updated form

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,35 @@
-const submit = document.getElementById('submit')
+const form = document.getElementById('contact-form')
+const status = document.getElementById('submit-status')
 
-
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+  
+    let data = new FormData(event.target)
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.style.opacity = '1'
+        status.innerHTML = 'Sent 😁'
+        setTimeout(() => {
+            status.style.opacity = '0'
+            setTimeout(() => {
+                status.style.display = 'none'
+                form.reset()
+            }, 1000)
+        }, 2000)
+    }).catch(error => {
+        status.style.opacity = '1'
+        status.innerHTML = '&#9888;'
+        setTimeout(() => {
+            status.style.opacity = '0'
+            setTimeout(() => {
+                status.style.display = 'none'
+                form.reset()
+            }, 1000)
+        }, 2000)
+    })
+})
