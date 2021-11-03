48c5b6af45150836a13be1d07f3cf41bd5ee195a
anatolymakeyev <anatoly.makeyev@cloudbeat.io>
2021-11-03 18:01:52 +0100

    added popup message

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,15 @@
+const url = location.href
+const softRed = 'rgb(210, 60, 60)'
+const softGreen = 'rgb(35, 210, 65)'
+
+const popUp = document.querySelector('.popup')
+const overlay = document.querySelector('.overlay')
 const form = document.getElementById('contact-form')
-const status = document.getElementById('submit-status')
+const statusIcon = document.getElementById('status-icon')
+const statusPopup = document.getElementById('status-popup')
+const statusMessage = document.getElementById('submit-status')
+const statusHeader = document.getElementById('submit-status-header')
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -12,26 +22,48 @@ form.addEventListener('submit', (event) => {
             'Accept': 'application/json'
         }
     }).then(response => {
-        status.style.color = 'var(--soft-green);'
-        status.style.opacity = '1'
-        status.innerHTML = 'Sent 😁'
+        statusIcon.innerHTML = '&#129297;'
+        statusHeader.innerHTML = 'Your message has been sent!'
+        statusMessage.innerHTML = 'We will start saving you money as soon as possible'
+        statusIcon.style.fontSize = '50px'
+        statusIcon.style.color = softGreen
+        statusHeader.style.color = softGreen
+        popUp.style.border = `2px solid ${softGreen}`
+        statusIcon.style.opacity = '1'
+        statusHeader.style.opacity = '1'
+        statusMessage.style.opacity = '1'
+        
         setTimeout(() => {
-            status.style.opacity = '0'
+            overlay.style.opacity = '0'
+            popUp.style.opacity = '0'
             setTimeout(() => {
-                status.style.display = 'none'
+                overlay.style.display = 'none'
+                popUp.style.display = 'none'
                 form.reset()
+                window.location.href = url
             }, 1000)
-        }, 2000)
+        }, 3000)
+
     }).catch(error => {
-        status.style.color = 'var(--soft-red);'
-        status.style.opacity = '1'
-        status.innerHTML = '⚠'
+        statusIcon.innerHTML = '&#9888;'
+        statusHeader.innerHTML = 'Oops!'
+        statusMessage.innerHTML = 'There seems to be a problem with your internet connection'
+        statusIcon.style.fontSize = '50px'
+        statusIcon.style.color = softRed
+        statusHeader.style.color = softRed
+        popUp.style.border = `2px solid ${softRed}`
+        statusIcon.style.opacity = '1'
+        statusHeader.style.opacity = '1'
+        statusMessage.style.opacity = '1'
+        
         setTimeout(() => {
-            status.style.opacity = '0'
+            overlay.style.opacity = '0'
+            popUp.style.opacity = '0'
             setTimeout(() => {
-                status.style.display = 'none'
+                overlay.style.display = 'none'
+                popUp.style.display = 'none'
                 form.reset()
             }, 1000)
-        }, 2000)
+        }, 3000)
     })
 })
