0756d03db8d7d95a6336356692547c553055b7f0
Anatoly Makeyev <anatoly.makeyev@gmail.com>
2021-11-05 18:55:42 +0200

    updated contact form

--- src/contact.js
+++ src/contact.js
@@ -1,16 +1,42 @@
 const url = window.location.href
-const softRed = 'rgb(210, 60, 60)'
-const softGreen = 'rgb(35, 210, 65)'
 
-const popUp = document.querySelector('.popup')
-const overlay = document.querySelector('.overlay')
-const form = document.getElementById('contact-form')
-const statusIcon = document.getElementById('status-icon')
-const statusPopup = document.getElementById('status-popup')
-const statusMessage = document.getElementById('submit-status')
-const statusHeader = document.getElementById('submit-status-header')
+function getXPath(path) {
+    return document.evaluate(
+        path, document, null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE, null
+    ).singleNodeValue
+}
 
+// lift labels
+const inputs = document.getElementsByClassName('form-input').length
 
+for (let x = 1; x <= inputs; x++) {
+    let input = '//*[@class="form-input"]'
+    let label = '//*[@class="form-label"]'
+
+    getXPath(`(${input})[${x}]`).addEventListener('focus', () => {
+        getXPath(`(${input})[${x}]`).style.border = `2px solid ${softBlue}`
+        getXPath(`(${label})[${x}]`).style.color = softBlue
+        getXPath(`(${label})[${x}]`).style.left = '0'
+        getXPath(`(${label})[${x}]`).style.top = '0'
+        setTimeout(() => { 
+            getXPath(`(${label})[${x}]`).innerHTML += ':'
+        }, 250)
+    })
+
+    getXPath(`(${input})[${x}]`).addEventListener('blur', () => {
+        getXPath(`(${input})[${x}]`).style.border = `1px solid ${softBlack}`
+        getXPath(`(${label})[${x}]`).style.color = softGrey
+        getXPath(`(${label})[${x}]`).style.left = '20px'
+        getXPath(`(${label})[${x}]`).style.top = '35px'
+        setTimeout(() => { 
+            let text = getXPath(`(${label})[${x}]`).innerText.slice(0, -1)
+            getXPath(`(${label})[${x}]`).innerText = text
+        }, 250)
+    })
+}
+
+// submit form
 form.addEventListener('submit', (event) => {
     event.preventDefault()
   
@@ -21,6 +47,7 @@ form.addEventListener('submit', (event) => {
         headers: {
             'Accept': 'application/json'
         }
+        
     }).then(response => {
         statusIcon.innerHTML = '&#129297;'
         statusHeader.innerHTML = 'Message has been sent!'
@@ -67,3 +94,4 @@ form.addEventListener('submit', (event) => {
         }, 3000)
     })
 })
+
