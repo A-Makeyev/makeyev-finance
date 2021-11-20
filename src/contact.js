0a00a6afe2964970427a594327c33c9c0a05caca
anatolymakeyev <anatoly.makeyev@cloudbeat.io>
2021-11-21 00:40:12 +0200

    updated contact form

--- src/contact.js
+++ src/contact.js
@@ -43,7 +43,7 @@
 // reset labels after submit
 function resetLabels() {
     for (let x = 1; x <= formInputs.length; x++) {
-        getXPath(`(${inputXPath})[${x}]`).style.boxShadow = `0 4px 2px -2px ${softRed}`
+        getXPath(`(${inputXPath})[${x}]`).style.boxShadow = `0 4px 2px -2px ${softBlack}`
         getXPath(`(${inputXPath})[${x}]`).style.border = `1px solid ${softBlack}`
         getXPath(`(${labelXPath})[${x}]`).style.color = softGrey
         getXPath(`(${labelXPath})[${x}]`).style.left = '20px'
